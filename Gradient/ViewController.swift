34693f49b6787ab6dd8505257dce11e057e7cfbc
Roman <j@tjo.biz>
2021-04-17 00:49:03 +0800

    Add more gentle shades

--- Gradient/ViewController.swift
+++ Gradient/ViewController.swift
@@ -17,15 +17,17 @@ class ViewController: UIViewController {
 
     override func viewDidAppear(_ animated: Bool) {
         super .viewDidAppear(animated)
-        let firstColor = #colorLiteral(red: 0.8661295573, green: 0.9861111111, blue: 0.8010525174, alpha: 0.7506956336).cgColor
+        let firstColor = #colorLiteral(red: 0.8927571097, green: 0.9861111111, blue: 0.9197431438, alpha: 1).cgColor
         let secondColor = #colorLiteral(red: 1, green: 0.9710286458, blue: 0.9067925347, alpha: 0.8543717894).cgColor
         let thirdColor = #colorLiteral(red: 0.9220648872, green: 1, blue: 1, alpha: 1).cgColor
-        let forthColor = #colorLiteral(red: 1, green: 0.9358995226, blue: 1, alpha: 1).cgColor
+        let forthColor = #colorLiteral(red: 0.9906512106, green: 0.9572585587, blue: 1, alpha: 1).cgColor
         let fifthColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1).cgColor
-        
         let gradientLayer = CAGradientLayer()
         gradientLayer.frame = view.bounds
-        gradientLayer.colors = [firstColor, secondColor, thirdColor, forthColor, firstColor]
+        gradientLayer.colors = [ fifthColor, secondColor, firstColor, thirdColor, forthColor, fifthColor]
+        gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.0)
+        gradientLayer.endPoint = CGPoint(x: 1.0, y: 1.0)
+        gradientLayer.locations = [0.1, 0.25, 0.4, 0.6, 0.8, 0.9, 1.0]
         //view.layer.addSublayer(gradientLayer)
         view.layer.insertSublayer(gradientLayer, at: 0)
     }
