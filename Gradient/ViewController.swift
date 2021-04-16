e9fac3ef41fb7942aba1540981bafda4cb3ce9c0
Roman <j@tjo.biz>
2021-04-16 21:59:03 +0800

    Initial Commit

---
+++ Gradient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gradient
+//
+//  Created by Joker on 4/16/21.
+//  Copyright © 2021 Elena Prokofeva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
